582012816c0ca662fc313579ccca89e7835187b9
Arsh Tiwari <dchglucknow@gmail.com>
2024-09-02 21:19:41 +0530

    frontend utils

--- annual-report-portal/src/components/Navbar.jsx
+++ annual-report-portal/src/components/Navbar.jsx
@@ -3,17 +3,18 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-600 p-4">
+    <nav className="bg-gray-100 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-white text-xl font-bold">Annual Report Portal</h1>
+        <h1 className="text-black text-xl font-bold">InsightEd</h1>
         <div>
-          <Link className="text-white mx-2" to="/">Dashboard</Link>
-          <Link className="text-white mx-2" to="/reports">Reports</Link>
-          <Link className="text-white mx-2" to="/login">Login</Link>
+          <Link className="text-black mx-8" to="/">Home</Link>
+          <Link className="text-red-800 mx-8" to="/login">Sign In</Link>
+          <Link className="bg-red-500 text-white px-4 py-2 rounded-md mx-6" to="/signup">Sign Up</Link>
         </div>
       </div>
     </nav>
   );
 };
+
 
 export default Navbar;
